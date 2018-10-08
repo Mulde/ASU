20c1cd3ea9c7a46cb00e25a1fd03ee3ef70178b6
Mads Villadsen <Mads.villadsen@hotmail.com>
2018-10-08 09:52:00 +0200

    implementation of network controls
    started on the real network controls by setting up a command enum file and the client

--- eclipse-workspace/Olregnskab2/src/MHVDKASU/Server.java
+++ eclipse-workspace/Olregnskab2/src/MHVDKASU/Server.java
@@ -1,35 +1,38 @@
 package MHVDKASU;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 
-public class Server {
-	private static int LEN = 1024;
-	private static ServerSocket	server;
-	private static byte[] b = new byte[LEN];
+public class Server implements Runnable {
+	ServerSocket server;
+	private HashMap<String,HashMap> tap = new HashMap<String, HashMap>();
+	private List<String> drinks = new LinkedList<String>();
 	
-	public static void main(String[] args) throws IOException{
-		server = new ServerSocket(8090);
-		while(true) {
-			Socket sck = server.accept();
-			sck.getInputStream().read(b,0,LEN);
-			String s = new String(b);
-			switch(order) {
-			case 1: s = "add person";
-					
-					break;
-			case 2: s = "add drink";
-					
-					break;
-			case 3: s = "cheers";
-					
-					break;
-			}
-		}
+	public Server(String string, int i) throws IOException {
+		server = new ServerSocket();
+		server.bind(new InetSocketAddress(i));
+		
 	}
-
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		try {
+			Socket sck  = server.accept();
+			InputStreamReader st = new InputStreamReader(sck.getInputStream());
+			BufferedReader br = new BufferedReader(st);
+		}
+		
+	}
+	
 }
--- eclipse-workspace/Olregnskab2/src/MHVDKASU/socket.java
+++ eclipse-workspace/Olregnskab2/src/MHVDKASU/socket.java
@@ -1,23 +1,26 @@
 package MHVDKASU;
 
 import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 
 public class socket {
-	public static void main(String[] args) throws UnknownHostException, IOException {	
-		Socket sck = new Socket("localhost",8090);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("enter string");
-		String s = br.readLine();
-		sck.getOutputStream().write(s.getBytes());
-		byte[] b = new byte[1024];
-		sck.getInputStream().read(b );
-		System.out.println(new String(b));
-		sck.close();
+	private Socket sck;
+	public socket(String string, int i) throws UnknownHostException, IOException {
+		sck = new Socket(string ,i);
+	}
+	public void addperson(String string) throws IOException {
+		String str = commands.Add_Person.getCommand()+":"+string+"\n";
+		sck.getOutputStream().write(str.getBytes());
+	}
+	public void adddrink(String person, String drink, Integer number) throws IOException {
+		String str = commands.Buy_Drink.getCommand()+":"+person+" "+drink+" "+number+"\n";
+		sck.getOutputStream().write(str.getBytes());
+	}
+	public void cheers(String string) throws IOException {
+		String str = commands.Cheers.getCommand()+":"+string+"\n";
+		sck.getOutputStream().write(str.getBytes());
 	}
 
 }
